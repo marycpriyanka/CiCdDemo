aff818780d204642fc093ef0ef4a92e29a3b349a
marycpriyanka <marycpriyanka@gmail.com>
2022-11-02 13:32:00 -0700

    Fixed fail test

--- hello-circle/src/main/java/com/trilogyed/hellocircle/controller/HelloCircleController.java
+++ hello-circle/src/main/java/com/trilogyed/hellocircle/controller/HelloCircleController.java
@@ -9,4 +9,9 @@ public class HelloCircleController {
     public String helloCircle(){
         return "Hello, Circle!";
     }
+
+    @GetMapping(value = "/answer")
+    public String getAnswer(){
+        return "The answer is 42!";
+    }
 }
